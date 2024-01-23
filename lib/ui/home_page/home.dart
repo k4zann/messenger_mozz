7abd4990892751b1ea6892e84613847919f6d765
Arshat Aitkozha <arshikoffice@gmail.com>
2024-01-23 17:42:14 +0600

    added simple interface to homePage(the list of users)

--- lib/ui/home_page/home.dart
+++ lib/ui/home_page/home.dart
@@ -3,14 +3,59 @@ import 'package:flutter/material.dart';
 import 'package:messenger_mozz/services/auth_service.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key, });
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _searchController = TextEditingController();
+
+  Container _searchField() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _searchController,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'Поиск',
+          hintStyle: const TextStyle(
+            color: Color(0xff9DB7CB),
+            fontSize: 14,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Image(
+              image: AssetImage('assets/search_s.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Messenger Mozz'),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: const Text(
+              'Чаты',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 32
+            )
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () {
@@ -21,11 +66,25 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Column(
+        children: [
+          _searchField(),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Чат $index'),
+                  subtitle: Text('Последнее сообщение'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/chat');
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
